5e0eeab2e060cd727823dd1c4b39a88515f12d99
RoloHPolo <robinholopolo@gmail.com>
2026-05-01 20:12:31 +0100

    Light+Rotation Controls, SFX, visual overhaul
    - Used lil-gui to add a control panel for lighting and rotation settings
    - Added sound effects on button presses (model loading buttons have their own distinct SFX to other buttons)
    - Visual overhaul (getting rid of coke/sprite/dr pepper branding, changing colour scheme, general CSS tweaks)

--- 3dapp/assignment/js/script.js
+++ 3dapp/assignment/js/script.js
@@ -1,34 +1,60 @@
 let scene, camera, renderer, clock, mixer, actions = [], mode, isWireframe = false;
 let loadedModel;
+let isRotating = false;
+let rotationSettings = {
+  speed: 0.02,
+  axis: "y"
+};
+let ambientLight, directionalLight, fillLight;
+let gui;
+
+
 const assetPath = './assets/models/';
 const loader = new THREE.GLTFLoader();
+const buttonSound = new Audio('./assets/sounds/buttonclick.mp3');
+const buttonModelSound = new Audio('./assets/sounds/buttonclickmodel.mp3');
+buttonSound.volume = 0.5;
 
+class ColorGUIHelper {
+    constructor(object, prop) {
+      this.object = object;
+      this.prop = prop;
+    }
+    get value() {
+      return this.object[this.prop].getHexString();
+    }
 
-init();
+    set value(hexString) {
+      this.object[this.prop].set(hexString);
+    }
+}
 
 function init(){
 
   clock = new THREE.Clock();
 
   scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x00aaff);
+  scene.background = new THREE.Color(0x000000);
 
   camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.position.set(-5, 25, 20);
   
-  const ambient = new THREE.AmbientLight(0xffffff, 2.5);
-  scene.add(ambient);
+  ambientLight = new THREE.AmbientLight(0xffffff, 2.5);
+  scene.add(ambientLight);
 
-  const directional = new THREE.DirectionalLight(0xffffff, 3);
-  directional.position.set(5, 10, 7);
-  scene.add(directional);
+  directionalLight = new THREE.DirectionalLight(0xffffff, 3);
+  directionalLight.position.set(5, 10, 7);
+  scene.add(directionalLight);
 
-  const fillLight = new THREE.DirectionalLight(0xffffff, 1.5);
+  fillLight = new THREE.DirectionalLight(0xffffff, 1.5);
   fillLight.position.set(-5, 5, -5);
   scene.add(fillLight);
 
+  gui = new lil.GUI();
+  setupLightingGUI();
+
   const canvas = document.getElementById('threeContainer');
-  renderer = new THREE.WebGLRenderer({ canvas: canvas });
+  renderer = new THREE.WebGLRenderer({ canvas: canvas});
   renderer.setPixelRatio(window.devicePixelRatio);
   resize();
 
@@ -46,10 +72,23 @@ function init(){
   animate();
 }
 
+function playButtonSound() {
+  buttonSound.currentTime = 0;
+  buttonSound.play();
+}
+
+function playModelButtonSound() {
+  buttonModelSound.currentTime = 0;
+  buttonModelSound.play();
+}
+
 function setupButtons() {
+
+
   const btn = document.getElementById('btn');
   if (btn) {
     btn.addEventListener('click', function () {
+      playButtonSound();
       if (actions.length > 0) {
         actions.forEach(action => {
           action.reset();
@@ -67,6 +106,7 @@ function setupButtons() {
   const wireframeBtn = document.getElementById('toggleWireframe');
   if (wireframeBtn) {
     wireframeBtn.addEventListener('click', function () {
+      playButtonSound();
       isWireframe = !isWireframe;
       toggleWireframe(isWireframe);
     });
@@ -75,25 +115,28 @@ function setupButtons() {
   const rotateBtn = document.getElementById('rotate');
   if (rotateBtn) {
     rotateBtn.addEventListener('click', function() {
-      if (loadedModel) {
-        const axis = new THREE.Vector3(0, 1, 0);
-        const angle = Math.PI / 8;
-        loadedModel.rotateOnAxis(axis, angle);
-      } else {
-        console.warn('Model not loaded');
-      }
+      playButtonSound();
+      isRotating = !isRotating;
     });
   }
 
   const canBtn = document.getElementById('loadCan');
   if (canBtn) {
-    canBtn.addEventListener('click', () => loadModel('can.glb'));
+    canBtn.addEventListener('click', function() {
+      playModelButtonSound();
+      loadModel('can.glb');
+    }); 
   }
 
   const cubeBtn = document.getElementById('loadCube');
   if (cubeBtn) {
-    cubeBtn.addEventListener('click', () => loadModel('rubiks.glb'));
+    cubeBtn.addEventListener('click', function() {
+      playModelButtonSound();
+      loadModel('rubiks.glb');
+    });
   }
+
+  
 }
 
 function animate(){
@@ -101,6 +144,24 @@ function animate(){
 
   if (mixer) {
     mixer.update(clock.getDelta());
+  }
+
+  let axis;
+
+  switch (rotationSettings.axis) {
+    case "x":
+      axis = new THREE.Vector3(1, 0, 0);
+      break;
+    case "y":
+      axis = new THREE.Vector3(0, 1, 0);
+      break;
+    case "z":
+      axis = new THREE.Vector3(0, 0, 1);
+      break;
+  }
+
+  if (isRotating && loadedModel) {
+    loadedModel.rotateOnAxis(axis, rotationSettings.speed);
   }
 
   renderer.render(scene, camera);
@@ -127,6 +188,7 @@ function resize() {
   camera.updateProjectionMatrix();
 
   renderer.setSize(width, height, false);
+
 }
 
 function update(){
@@ -136,7 +198,7 @@ function update(){
 }
 
 function loadModel(modelFile) {
-  
+  isRotating = false;
   if (loadedModel) {
     scene.remove(loadedModel);
     loadedModel = null;
@@ -157,7 +219,34 @@ function loadModel(modelFile) {
       actions.push(action);
     });
 
-  });
-
-  
+  }); 
 }
+
+function setupLightingGUI() {
+  const ambientFolder = gui.addFolder("Ambient Light");
+  ambientFolder.addColor(new ColorGUIHelper(ambientLight, 'color'), 'value').name('Colour');
+  ambientFolder.add(ambientLight, 'intensity', 0, 5, 0.1);
+  ambientFolder.open();
+
+  const directionalFolder = gui.addFolder("Directional Light");
+  directionalFolder.addColor(new ColorGUIHelper(directionalLight, 'color'), 'value').name('Colour');
+  directionalFolder.add(directionalLight, 'intensity', 0, 5, 0.1);
+  directionalFolder.add(directionalLight.position, 'x', -20, 20, 0.1);
+  directionalFolder.add(directionalLight.position, 'y', -20, 20, 0.1);
+  directionalFolder.add(directionalLight.position, 'z', -20, 20, 0.1);
+  directionalFolder.open();
+
+  const fillFolder = gui.addFolder("Fill Light");
+  fillFolder.addColor(new ColorGUIHelper(fillLight, 'color'), 'value').name('Colour');
+  fillFolder.add(fillLight, 'intensity', 0, 5, 0.1);
+  fillFolder.add(fillLight.position, 'x', -20, 20, 0.1);
+  fillFolder.add(fillLight.position, 'y', -20, 20, 0.1);
+  fillFolder.add(fillLight.position, 'z', -20, 20, 0.1);
+
+  const rotationFolder = gui.addFolder("Rotation");
+  rotationFolder.add(rotationSettings, "speed", 0, 0.15, 0.01).name("Speed");
+  rotationFolder.add(rotationSettings, "axis", ["x", "y", "z"]).name("Axis");
+  rotationFolder.open();
+}
+
+init();
